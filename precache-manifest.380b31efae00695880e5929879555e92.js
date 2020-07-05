4d4aae4f88274b7741fd9641d4c9e693971081ce
Sean Keever <swkeever@gmail.com>
2020-07-05 11:50:42 -0700

    Updates

---
+++ precache-manifest.380b31efae00695880e5929879555e92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b189c8609d3b0f414e309d75f16aae46",
+    "url": "/color-generator/index.html"
+  },
+  {
+    "revision": "ad9f2cae481d4b1d5e84",
+    "url": "/color-generator/static/js/2.bdddf9de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/color-generator/static/js/2.bdddf9de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "821af66da6fefa3e666a",
+    "url": "/color-generator/static/js/main.6fc2154c.chunk.js"
+  },
+  {
+    "revision": "a1cfb88a1dc1d9e37dd7",
+    "url": "/color-generator/static/js/runtime-main.8a33e40c.js"
+  }
+]);
